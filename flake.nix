012d8bc587bcb6b4d56104a04c63a6ac13dd5caf
Krutonium <PFCKrutonium@gmail.com>
2022-03-31 01:45:56 -0400

    Even better

--- flake.nix
+++ flake.nix
@@ -1,62 +1,48 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
 
   outputs = {
     self,
     nixpkgs,
-    ...
   }: {
-    devShell = nixpkgs.lib.genAttrs ["x86_64-linux"] (system: let
+    apps = nixpkgs.lib.genAttrs ["x86_64-linux"] (system: {
+      default = {
+        type = "app";
+        program = self.packages.${system}.default + "/bin/sonix-flasher";
+      };
+    });
+
+    packages = nixpkgs.lib.genAttrs ["x86_64-linux"] (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-      pkgs.mkShell {
-        packages = [
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        name = "sonix-flasher";
+        nativeBuildInputs = with pkgs; [makeWrapper];
+        buildCommand = ''
+          makeWrapper ${self.packages.${system}.env}/bin/fbs $out/bin/sonix-flasher \
+            --prefix PATH : ${self.packages.${system}.env}/bin \
+            --add-flags run
+        '';
+      };
+      env = pkgs.buildEnv {
+        name = "sonix-flasher-env";
+        paths = [
           ((pkgs.python39.override {
-              packageOverrides = p-self: p-super: {
-                fbs = with p-super;
-                  buildPythonPackage rec {
-                    pname = "fbs";
-                    version = "0.8.6";
-                    propagatedBuildInputs = [p-self.pyinstaller];
-                    doCheck = false;
-                    src = fetchPypi {
-                      inherit pname version;
-                      sha256 = "sha256-hIGRJlAtlZBQJ8jGp4j5PzGPHADsFk3eXvBtup0ROc8=";
+              packageOverrides = p-self: p-super: let
+                buildPyPi = pname: version: propagatedBuildInputs: sha256:
+                  with p-super;
+                    buildPythonPackage rec {
+                      inherit pname version propagatedBuildInputs;
+                      doCheck = false;
+                      src = fetchPypi {
+                        inherit pname version sha256;
+                      };
                     };
-                  };
-                altgraph = with p-super;
-                  buildPythonPackage rec {
-                    pname = "altgraph";
-                    version = "0.17";
-                    src = fetchPypi {
-                      inherit pname version;
-                      sha256 = "sha256-HwWkcSJUL5cCjK94d1oJX75qJpm1CJ3oR361gxZ9aao=";
-                    };
-                  };
-                macholib = with p-super;
-                  buildPythonPackage rec {
-                    pname = "macholib";
-                    version = "1.14";
-                    propagatedBuildInputs = [p-self.altgraph];
-                    doCheck = false;
-                    src = fetchPypi {
-                      inherit pname version;
-                      sha256 = "sha256-DENryEfnsdm9oFYDUb9218r5MPtYWoKNE2CIOe9CxDI=";
-                    };
-                  };
-                pyinstaller = with p-super;
-                  buildPythonPackage rec {
-                    pname = "PyInstaller";
-                    version = "3.4";
-                    propagatedBuildInputs = with p-self; [altgraph pefile macholib];
-                    doCheck = false;
-                    src = fetchPypi {
-                      inherit pname version;
-                      sha256 = "sha256-pabgSmar/Ph2Homi662TeRnGvjOnuJY+GpYbVcs1mGs=";
-                    };
-                  };
+              in {
+                fbs = buildPyPi "fbs" "0.8.6" [p-self.pyinstaller] "sha256-hIGRJlAtlZBQJ8jGp4j5PzGPHADsFk3eXvBtup0ROc8=";
+                altgraph = buildPyPi "altgraph" "0.17" [] "sha256-HwWkcSJUL5cCjK94d1oJX75qJpm1CJ3oR361gxZ9aao=";
+                macholib = buildPyPi "macholib" "1.14" [p-self.altgraph] "sha256-DENryEfnsdm9oFYDUb9218r5MPtYWoKNE2CIOe9CxDI=";
+                pyinstaller = buildPyPi "PyInstaller" "3.4" (with p-self; [altgraph pefile macholib]) "sha256-pabgSmar/Ph2Homi662TeRnGvjOnuJY+GpYbVcs1mGs=";
               };
             })
             .withPackages (p:
@@ -76,6 +62,7 @@
           pkgs.stdenv.cc
           pkgs.qt5.full
         ];
-      });
+      };
+    });
   };
 }
